bbd1287bb8180fd75297db1e1c45dd2c255284fa
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-02-03 11:45:00 -0300

    update tests descriptions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe User, type: :model do
   fixtures :users
 
   context 'data is valid' do
-    it 'User have a count of 3 after saving a new entrie' do
+    it 'return a count of 3 after saving a new entry in the User table' do
       user = User.new( name: "User_03", age: 21, bio: "Lorem Ipsum" ).save
       expect(User.all.count).to eq(3)
     end
   end
 
   context 'data is invalid' do
-    it 'user is invalid when name is blank' do
+    it 'return a error when user name is blank' do
       user = User.new( bio: "Lorem Ipsum" )
       expect(user).to be_invalid
     end
 
-    it 'user is invalid when name has already been taken' do
+    it 'return a error when user name has already been taken' do
       user = User.new( name: "User_01" )
       expect(user).to be_invalid
     end
