4011b33663d3629873bc4304ee901706ea956ece
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-02-03 16:09:41 -0300

    create telephone model validations

--- app/models/telephone.rb
+++ app/models/telephone.rb
@@ -1,3 +1,6 @@
 class Telephone < ApplicationRecord
   belongs_to :contact
+  validates :phone_number, presence: true, uniqueness: true
+  validates :phone_type, presence: true
+  validates :main_phone, inclusion: { in: [true, false] }, uniqueness: { scope: :contact_id, if: :main_phone }
 end
--- db/migrate/20220203150416_create_telephones.rb
+++ db/migrate/20220203150416_create_telephones.rb
@@ -1,12 +1,14 @@
 class CreateTelephones < ActiveRecord::Migration[6.1]
   def change
     create_table :telephones do |t|
-      t.integer :phone_number
-      t.string :phone_type
-      t.boolean :main_phone
+      t.integer :phone_number, null: false
+      t.string :phone_type, null: false
+      t.boolean :main_phone, null: false
       t.references :contact, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_index :telephones, :phone_number, unique: true
+    add_index :telephones, [:main_phone, :contact_id], unique: true, where: "main_phone IS TRUE"
   end
 end
