5e021bc26a73ddd2216ace52d3d10e873ce9db7c
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-02-03 11:47:36 -0300

    create tests for Contact model

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :contacts
+
+  context 'data is valid' do
+    it 'return a count of 3 after saving a new entry in the Contact table' do
+      contact = Contact.new( name: "Contact_03", user_id: 1).save
+      expect(Contact.all.count).to eq(3)
+    end
+
+    context 'data is invalid' do
+      it 'return a error when contact name is blank' do
+        contact = Contact.new( birthday: "1990-01-01", user_id: 1 )
+        expect(contact).to be_invalid
+      end
+  
+      it 'return a error when contact name has already been taken for another contact on that user' do
+        contact = Contact.new( name: "Contact_01", user_id: 1 )
+        expect(contact).to be_invalid
+      end
+    end
+  end
 end
