036f150fd0f9f6c893171a281b732126a3c3ea23
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-02-11 12:08:06 -0300

    change phone_number type to string

--- db/migrate/20220203150416_create_telephones.rb
+++ db/migrate/20220203150416_create_telephones.rb
@@ -1,7 +1,7 @@
 class CreateTelephones < ActiveRecord::Migration[6.1]
   def change
     create_table :telephones do |t|
-      t.integer :phone_number, null: false
+      t.string :phone_number, null: false
       t.string :phone_type, null: false
       t.boolean :main_phone, null: false
       t.references :contact, null: false, foreign_key: true
--- spec/models/telephone_spec.rb
+++ spec/models/telephone_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Telephone, type: :model do
 
   context 'data is valid' do
     it 'return a count of 5 after saving a new entry in the Telephone table' do
-      telephone = Telephone.new( phone_number: 963852741, phone_type: "home", main_phone: false, contact_id: 1 ).save
+      telephone = Telephone.new( phone_number: "963852741", phone_type: "home", main_phone: false, contact_id: 1 ).save
       expect(Telephone.all.count).to eq(5)
     end
   end
@@ -17,32 +17,32 @@ RSpec.describe Telephone, type: :model do
     end
 
     it 'return a validation error when telephone phone_type is blank' do
-      telephone = Telephone.new( phone_number: 963852741, main_phone: false, contact_id: 1 )
+      telephone = Telephone.new( phone_number: "963852741", main_phone: false, contact_id: 1 )
       expect(telephone).to be_invalid
     end
 
     it 'return a validation error when telephone main_phone is blank' do
-      telephone = Telephone.new( phone_number: 963852741, phone_type: "home", contact_id: 1 )
+      telephone = Telephone.new( phone_number: "963852741", phone_type: "home", contact_id: 1 )
       expect(telephone).to be_invalid
     end
 
     it 'return a validation error when the contact already has a main_phone' do
-      telephone = Telephone.new( phone_number: 963852741, phone_type: "home", main_phone: true, contact_id: 1 )
+      telephone = Telephone.new( phone_number: "963852741", phone_type: "home", main_phone: true, contact_id: 1 )
       expect(telephone).to be_invalid
     end
 
     it 'return a validation error when the phone_number has already been taken by that contact' do
-      telephone = Telephone.new( phone_number: 123456789, phone_type: "home", main_phone: false, contact_id: 1 )
+      telephone = Telephone.new( phone_number: "123456789", phone_type: "home", main_phone: false, contact_id: 1 )
       expect(telephone).to be_invalid
     end
 
     it 'return a validation error when trying to assign a telephone to a contact that doesn\'t exist' do
-      telephone = Telephone.new( phone_number: 963852741, phone_type: "home", main_phone: false, contact_id: 8)
+      telephone = Telephone.new( phone_number: "963852741", phone_type: "home", main_phone: false, contact_id: 8)
       expect(telephone).to be_invalid
     end
 
     it 'return a validation error when not assigned to a contact' do
-      telephone = Telephone.new( phone_number: 963852741, phone_type: "home", main_phone: false)
+      telephone = Telephone.new( phone_number: "963852741", phone_type: "home", main_phone: false)
       expect(telephone).to be_invalid
     end
   end
