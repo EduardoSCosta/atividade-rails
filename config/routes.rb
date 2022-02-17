5861c5cee3f3d802e04001f40a946dfa60f24180
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-02-17 17:51:03 -0300

    update delete user route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post 'users' => 'users#create'
   get 'users/new' => 'users#new', as: 'user_new'
   patch 'users/:id' => 'users#update'
-  # get 'users/destroy'
+  delete 'users/:id' => 'users#destroy'
   get 'users/:id/edit'  => 'users#edit', as: 'user_edit'
   get 'users/:id' => 'users#show', as: 'user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
