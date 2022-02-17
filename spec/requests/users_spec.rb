03cec758f833cb46b632e4917f170176f09abb58
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-02-17 12:10:55 -0300

    update links to use path helpers

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -38,30 +38,30 @@ RSpec.describe "Users", type: :request do
 
   describe "POST /users" do
     it "returns http created" do
-      post "/users", params: {user: {name: "User Test", age: 42, bio: "This is a test biography"}}
+      post users_path, params: {user: {name: "User Test", age: 42, bio: "This is a test biography"}}
       expect(response).to have_http_status(:found)
     end
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get users_edit_path
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /edit" do
+  #   it "returns http success" do
+  #     get "/users/edit"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get users_update_path
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /update" do
+  #   it "returns http success" do
+  #     get "/users/update"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get users_destroy_path
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /destroy" do
+  #   it "returns http success" do
+  #     get "/users/destroy"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
